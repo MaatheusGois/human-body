520c89ad660ee4b8c904e4de8e97da045fc07b6c
Gois <matheusgoislimasilva@gmail.com>
2021-03-26 22:01:34 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,39 @@
-$(document).ready(function() {
-    var currentColour = "#F3B9B3";
+var color1 = "#FFFFFF";
+var color2 = "#F7D9DE";
+var color3 = "#F3B9B3";
+var color4 = "#F19894";
+var color5 = "#ED7175";
 
-    $("svg").each(function(){
-      var picture = this;
-  
-      $.each(picture.childNodes, function(i, e){
-        $(e).on("click", function(e) {
-          $(this).attr("fill", currentColour);
+$(document).ready(function () {
+    $("svg").each(function () {
+        var picture = this;
+        $.each(picture.childNodes, function (i, e) {
+            $(e).on("click", function (e) {
+                let newColor = changeState($(this).attr("fill"))
+                $(this).attr("fill", newColor);
+            });
         });
-      });
     });
-  });
+
+    function changeState(color) {
+        switch (color) {
+            case color1:
+                return color2
+            case "white":
+                return color2
+            case "none":
+                return color2
+
+            case color2:
+                return color3
+            case color3:
+                return color4
+            case color4:
+                return color5
+            case color5:
+                return color1
+            default:
+                return color
+        }
+    }
+});
